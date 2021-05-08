60e30edc8fa90f5ba349fb9b594c7e6d06f772cb
JP1998 <littlebigjonny06@gmail.com>
2021-05-08 21:14:24 +0200

    Add print function which prints addon name before the message

--- BattlePetWorldQuestTracker.lua
+++ BattlePetWorldQuestTracker.lua
@@ -2,11 +2,15 @@
 local app = select(2, ...);
 local L = app.L;
 
+app.print = function(self, ...)
+    print("[", L["TITLE"], "]: ", ...);
+end
+
 SLASH_BattlePetWorldQuestTracker1 = "/battlepetworldquesttracker";
 SLASH_BattlePetWorldQuestTracker2 = "/battlepetwqtracker";
 SLASH_BattlePetWorldQuestTracker3 = "/bpwqt";
 SlashCmdList["BattlePetWorldQuestTracker"] = function(cmd)
-    print("You typed '/bpwqt' or something similar.");
+    app.print("You typed '/bpwqt' or something similar.");
 end
 
 app:RegisterEvent("QUEST_LOG_UPDATE");
@@ -15,14 +19,14 @@ app:RegisterEvent("SEND_MAIL_SUCCESS");
 app:RegisterEvent("SEND_MAIL_FAILED");
 
 app.events.QUEST_LOG_UPDATE = function(...)
-    print("Quest log has updated.");
+    app.print("Quest log has updated.");
 end
 app.events.MAIL_SHOW = function(...)
-    print("You opened your mail.");
+    app.print("You opened your mail.");
 end
 app.events.SEND_MAIL_SUCCESS = function(...)
-    print("You successfully sent mail.");
+    app.print("You successfully sent mail.");
 end
 app.events.SEND_MAIL_FAILED = function(...)
-    print("You tried to send mail, which failed.");
+    app.print("You tried to send mail, which failed.");
 end
